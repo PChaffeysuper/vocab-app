543ebd1d35cf2fc0738f0764e0aeeaa17b23a9c0
Paolo Chaffey <paolo@superthinkers.co.uk>
2019-08-07 15:18:29 +0100

    Add files via upload

---
+++ www/js/app.js
@@ -0,0 +1,58 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('todo', ['ionic'])
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore; 
+  $scope.tasks = [{title: "Unprecidented", description: "Not gonna happen.", sentence: "That firework  shootings were unpredicted"}, {title: "Unexpected", description: "Robert"}];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+       title: task.title,
+       description: task.description,
+       sentence: task.sentence,
+  })
+    $scope.taskModal.hide();
+
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.Keyboard) {
+      window.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+})
